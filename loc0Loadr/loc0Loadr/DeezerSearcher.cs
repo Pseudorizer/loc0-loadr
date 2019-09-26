ec3ce346a8c10e444d3a5b78cb3a1675741682cc
Loc0DeD <kaleb.medhurst@gmail.com>
2019-09-26 15:00:20 +0100

    Implemented 90% working simple search result browsing system

--- loc0Loadr/loc0Loadr/DeezerSearcher.cs
+++ loc0Loadr/loc0Loadr/DeezerSearcher.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using loc0Loadr.Enums;
 using Newtonsoft.Json.Linq;
@@ -8,12 +10,10 @@ namespace loc0Loadr
     internal class DeezerSearcher
     {
         private readonly DeezerHttp _deezerHttp;
-        private readonly AudioQuality _audioQuality;
 
-        public DeezerSearcher(DeezerHttp deezerHttp, AudioQuality audioQuality)
+        public DeezerSearcher(DeezerHttp deezerHttp)
         {
             _deezerHttp = deezerHttp;
-            _audioQuality = audioQuality;
         }
 
         public async Task<string> Search(string term, SearchType searchType)
@@ -23,11 +23,24 @@ namespace loc0Loadr
                 return string.Empty;
             }
 
+            var resultLimit = Configuration.GetValue<int>("maxSearchResults");
+
+            if (resultLimit <= 0)
+            {
+                resultLimit = 40;
+            }
+            else if (resultLimit > 500)
+            {
+                resultLimit = 500;
+            }
+
+            resultLimit = 20;
+
             JObject searchResults = await _deezerHttp.HitUnofficialApi("deezer.pageSearch", new JObject
             {
                 ["query"] = term,
                 ["start"] = 0,
-                ["nb"] = 40,
+                ["nb"] = resultLimit,
                 ["suggest"] = false,
                 ["artist_suggest"] = false,
                 ["top_tracks"] = false
@@ -71,12 +84,64 @@ namespace loc0Loadr
                     break;
             }
 
-            int r = results.Count / 10;
-            int p = 1;
-
-            while (p < r)
+            if (results.Count == 0)
             {
+                Helpers.RedMessage("No results found");
+                return string.Empty;
+            }
+
+            int r = results.Count / 10;
+            int p = 0;
+
+            while (true)
+            {
+                var skip = p * 10;
+
+                if (skip == results.Count)
+                {
+                    skip -= 10;
+                }
+
+                var g = results.Skip(skip).Take(10).ToList();
+                var gLength = g.Count;
+
+                if (gLength == 0)
+                {
+                    Helpers.RedMessage("No more results found");
+                    p = 0;
+                    continue;
+                }
+
+                Console.WriteLine($"\nPage {p + 1} | {p * 10}-{(p + 1) * 10}/{results.Count}");
+
+                var t = g.Select(x => x.OutputString).ToList();
+                t.Add("See ten more");
+                t.Add("Go back ten");
+                t.Add("Back to start");
+                t.Add("No valid results, exit");
                 
+                var e = Helpers.TakeInput(1, t.Count, t.ToArray());
+
+                if (e == (gLength + 1).ToString())
+                {
+                    p++;
+                }
+                else if (e == (gLength + 2).ToString())
+                {
+                    p = p - 1 < 0 ? 0 : p - 1;
+                }
+                else if (e == (gLength + 3).ToString())
+                {
+                    p = 0;
+                }
+                else if (e == (gLength + 4).ToString())
+                {
+                    return string.Empty;
+                }
+                else
+                {
+                    return g[int.Parse(e)].Id;
+                }
             }
 
             return "";
